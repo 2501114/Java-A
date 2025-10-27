c575052f9f4939e210dc9b607485288db1eebe11
2501114 <rajabov2006@kbu.ac.kr>
2025-10-27 16:28:56 +0900

    3

---
+++ week6/sample6-5/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        final int MACH = 340;
+        float fighter;
+        int result;
+
+        System.out.printf("우리 진투기의 속도는 ?");
+        fighter = keyboard.nextFloat();
+
+        result = (int) ((fighter * MACH * 365 * 24 * 60 * 60) / 1000);
+
+        System.out.printf("1 마하는 초당 %d m를 이동합니다.\n",MACH);
+        System.out.printf("%.1f 마하님 진투기가 1년간 간 거리는 %,d \u33a2 입니다.\n",fighter,result);
+    }
+}
